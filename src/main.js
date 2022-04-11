6461a8d8c06c30d29389df2f3a37181ac85a3217
Harya Kusuma <harya.sisinfo@gmail.com>
2022-04-11 19:34:15 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -14,6 +14,21 @@ import VueSession from "vue-session";
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
 Vue.use(VueSession);
+axios.interceptors.request.use(function (config) {
+  // Ambil token
+  const token = localStorage.getItem('token')
+
+  // Jika access token ada, set authorization header tiap request
+  if (token) {
+    config.headers.Authorization =  `Bearer ${token}`;
+  }
+
+  return config;
+}, function (error) {
+  // Do something with request error
+  //if (DEBUG) { console.error("Error", error); }
+  return Promise.reject(error);
+});
 
 new Vue({
   router,
